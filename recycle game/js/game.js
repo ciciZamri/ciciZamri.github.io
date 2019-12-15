57745cfb9a28e97089b46375fa6742f0e6d21592
cici <hammiziezamri@gmail.com>
2019-12-15 22:39:50 +0800

    change sprite

--- recycle game/js/game.js
+++ recycle game/js/game.js
@@ -1,98 +1,123 @@
 let scoreText = document.querySelector("#score");
 let coinText = document.querySelector("#coin");
+let paperCountText = document.querySelector("#paper-count");
+let bottleCountText = document.querySelector("#bottle-count");
+let canCountText = document.querySelector("#can-count");
 
 let gamestate = {
     score: 0,
     coin: 0,
     currentPage: 'homepage',
-    containerCount: 3,
-    containerSpace: 5
+    containerCapacity: 7,
+    paperCount: 0,
+    bottleCount: 0,
+    canCount: 0
 };
 
 class Scene {
     static gotofield() {
-        player.moveto(app.screen.width, app.screen.height - 200, () => {
-            gamestate.currentPage = 'field';
-            console.log("go to field");
-            app.stage.removeChild(homepage);
-            app.stage.addChild(field);
-            app.stage.addChild(homebtn);
-            app.stage.addChild(player.gameobj);
-            ItemManager.instantiate(30);
-            player.jumpto(0, app.screen.height/2);
-            player.moveto(30, app.screen.height/2, null);
+        player.moveto(app.screen.width - 310, 350, () => {
+            player.moveto(app.screen.width - 315, 180, () => {
+                gamestate.currentPage = 'field';
+                console.log("go to field");
+                app.stage.removeChild(homepage);
+                app.stage.addChild(field);
+                app.stage.addChild(homebtn);
+                app.stage.addChild(player.gameobj);
+                ItemManager.instantiate();
+                app.stage.addChild(inventoryContainer);
+                player.jumpto(0, app.screen.height / 2);
+                player.moveto(30, app.screen.height / 2, null);
+            });
         });
     }
 
     static gotoshop() {
-        player.moveto(120, 300, ()=>{
-            gamestate.currentPage = 'shop';
-            app.stage.removeChild(homepage);
-            app.stage.addChild(shop);
-            app.stage.addChild(homebtn);
-        });
+        if(player.gameobj.x > app.screen.width - 300){
+            player.moveto(app.screen.width - 310, 350, () => {
+                player.moveto(app.screen.width - 370, 350, () => {
+                    player.moveto(90, 450, () => {
+                        gamestate.currentPage = 'shop';
+                        app.stage.removeChild(homepage);
+                        app.stage.addChild(shop);
+                        app.stage.addChild(homebtn);
+                    });
+                });
+            });
+        }
+        else{
+            player.moveto(90, 450, () => {
+                gamestate.currentPage = 'shop';
+                app.stage.removeChild(homepage);
+                app.stage.addChild(shop);
+                app.stage.addChild(homebtn);
+            });
+        }
+        
     }
 
     static gotohomepage() {
         let ypos;
-        if(gamestate.currentPage === 'shop'){
+        if (gamestate.currentPage === 'shop') {
             Scene.loadhomepage();
         }
-        else{
-            if(gamestate.currentPage === 'river') ypos = 100;
-            else if(gamestate.currentPage === 'field') ypos = app.screen.height - 200;
+        else {
+            if (gamestate.currentPage === 'river') ypos = 350;
+            else if (gamestate.currentPage === 'field') ypos = 180;
 
-            player.moveto(0, app.screen.height/2, ()=>{
+            player.moveto(0, app.screen.height / 2, () => {
                 Scene.loadhomepage();
                 player.jumpto(app.screen.width, ypos);
-                player.moveto(app.screen.width-100, ypos, null);
+                player.moveto(app.screen.width - 100, ypos, null);
             });
         }
     }
 
-    static loadhomepage(){
+    static loadhomepage() {
         gamestate.currentPage = 'homepage';
         app.stage.addChild(homepage);
         app.stage.addChild(player.gameobj);
+        app.stage.addChild(inventoryContainer);
 
-        house.position.set(app.screen.width / 2, 100);
+        house.position.set(270, 300);
 
-        riverbtn.position.set(app.screen.width - 100, 100);
+        riverbtn.position.set(app.screen.width - 100, 400);
         riverbtn.interactive = true;
         riverbtn.buttonMode = true;
 
-        fieldbtn.position.set(app.screen.width - 100, 300);
-        fieldbtn.interactive = true;
-        fieldbtn.buttonMode = true;
+        bus.position.set(app.screen.width - 310, 180);
+        bus.interactive = true;
+        bus.buttonMode = true;
 
-        shopbtn.position.set(100, 300);
+        shopbtn.position.set(80, 450);
         shopbtn.interactive = true;
         shopbtn.buttonMode = true;
 
-        c1l1.position.set(app.screen.width / 2 - 150, 200);
-        c2l1.position.set(app.screen.width / 2 - 150, 300);
-        c3l1.position.set(app.screen.width / 2 - 150, 400);
+        c1l1.position.set(100, 200);
+        c2l1.position.set(200, 180);
+        c3l1.position.set(300, 200);
     }
 
     static gotoriver() {
-        player.moveto(app.screen.width, 100, () => {
+        player.moveto(app.screen.width - 100, 350, () => {
             gamestate.currentPage = 'river';
             app.stage.removeChild(homepage);
             app.stage.addChild(river);
             app.stage.addChild(homebtn);
             app.stage.addChild(player.gameobj);
             console.log("go to river");
-            player.jumpto(10, app.screen.height/2);
-            player.moveto(30, app.screen.height/2, null);
+            player.jumpto(10, app.screen.height / 2);
+            player.moveto(30, app.screen.height / 2, null);
+            app.stage.addChild(inventoryContainer);
         });
     }
 }
 
-class Item extends GameObject{
+class Item extends GameObject {
     type;
     price;
 
-    constructor(sprite, type){
+    constructor(sprite, type) {
         super(sprite);
         this.type = type;
         //console.log(this.type);
@@ -102,75 +127,94 @@ class Item extends GameObject{
         this.assignEventListener();
     }
 
-    assignPrice(){
-        switch(this.type){
+    assignPrice() {
+        switch (this.type) {
             case "paper": this.price = 15; break;
             case "bottle": this.price = 20; break;
             case "can": this.price = 30; break;
         }
     }
 
-    assignEventListener(){
+    assignEventListener() {
         console.log(this.type, this.price);
-        this.gameobj.on('pointerdown', ()=>{
-            app.stage.removeChild(this.gameobj);
-            gamestate.score += 30;
-            updateUI();
+        this.gameobj.on('pointerdown', () => {
+            player.moveto(this.gameobj.x, this.gameobj.y, () => {
+                app.stage.removeChild(this.gameobj);
+                gamestate.score += 30;
+                switch (this.type) {
+                    case "paper": gamestate.paperCount += 1; break;
+                    case "bottle": gamestate.bottleCount += 1; break;
+                    case "can": gamestate.canCount += 1; break;
+                }
+                updateUI();
+            });
             console.log(`${this.type} clicked ${this.gameobj.x} ${this.gameobj.y} ${this.price}`);
         });
     }
 
-    clickedListener(){
+    clickedListener() {
         console.log(`${this.type} clicked ${this.gameobj} ${this.gameobj} ${this.price}`);
     }
 }
 
-class ItemManager{
+class ItemManager {
     static cans = [];
     static bottles = [];
     static papers = [];
     static rawItems = [];
 
-    static instantiate(count){
-        for(let i = 0 ; i<count ; i++){
-            const random = generateRandom(0, 2)
-            const x = generateRandom(150, app.screen.width-100);
-            const y = generateRandom(50, app.screen.height-100);
-            switch(random){
+    static initialize(count) {
+        for (let i = 0; i < count; i++) {
+            const random = generateRandom(0, 2);
+            // const x = generateRandom(150, app.screen.width-100);
+            // const y = generateRandom(50, app.screen.height-100);
+            switch (random) {
                 case 0:
-                    const canSprite = new PIXI.Sprite(PIXI.loader.resources[`${url}/assets/can.png`].texture);
-                    canSprite.position.set(x, y);
-                    canSprite.scale.set(0.3, 0.3);
+                    const canSprite = new PIXI.Sprite(PIXI.loader.resources[`${url}/asset2/can.png`].texture);
+                    //canSprite.position.set(x, y);
+                    canSprite.scale.set(0.08, 0.08);
+                    canSprite.rotation = generateRandom(0, 180);
                     this.rawItems.push(new Item(canSprite, "can"));
                     //console.log("add can");
                     break;
                 case 1:
-                    const paperSprite = new PIXI.Sprite(PIXI.loader.resources[`${url}/assets/paper.png`].texture);
-                    paperSprite.position.set(x, y);
-                    paperSprite.scale.set(0.3, 0.3);
+                    const paperSprite = new PIXI.Sprite(PIXI.loader.resources[`${url}/asset2/paper.png`].texture);
+                    //paperSprite.position.set(x, y);
+                    paperSprite.scale.set(0.03, 0.03);
+                    paperSprite.rotation = generateRandom(0, 180);
                     this.rawItems.push(new Item(paperSprite, "paper"));
                     //console.log("add paper");
                     break;
                 case 2:
-                    const bottleSprite = new PIXI.Sprite(PIXI.loader.resources[`${url}/assets/bottle.png`].texture);
-                    bottleSprite.position.set(x, y);
-                    bottleSprite.scale.set(0.3, 0.3);
+                    const bottleSprite = new PIXI.Sprite(PIXI.loader.resources[`${url}/asset2/bottle.png`].texture);
+                    //bottleSprite.position.set(x, y);
+                    bottleSprite.scale.set(0.08, 0.08);
+                    bottleSprite.rotation = generateRandom(0, 180);
                     this.rawItems.push(new Item(bottleSprite, "bottle"));
                     //console.log("add bottle");
                     break;
             }
         }
-        for(let item of this.rawItems){
+    }
+
+    static instantiate() {
+        for (let item of this.rawItems) {
+            const x = generateRandom(100, app.screen.width - 100);
+            const y = generateRandom(50, app.screen.height - 100);
+            item.gameobj.position.set(x, y);
             app.stage.addChild(item.gameobj);
         }
     }
 }
 
-function generateRandom(from, to){
-    return random = Math.floor(Math.random()*(to-from+1))+from;
+function generateRandom(from, to) {
+    return random = Math.floor(Math.random() * (to - from + 1)) + from;
 }
 
-function updateUI(){
+function updateUI() {
     scoreText.innerHTML = `${gamestate.score}`;
     coinText.innerHTML = `${gamestate.coin}`;
+    paperCountText.innerHTML = `${gamestate.paperCount}/${gamestate.containerCapacity}`;
+    bottleCountText.innerHTML = `${gamestate.bottleCount}/${gamestate.containerCapacity}`;
+    canCountText.innerHTML = `${gamestate.canCount}/${gamestate.containerCapacity}`;
 }
--- recycle game/js/init.js
+++ recycle game/js/init.js
@@ -1,6 +1,6 @@
 let app = new PIXI.Application({
     width: 1000,
-    height: 500,
+    height: 550,
     antialias: true,
     resolution: 1,
 });
@@ -8,28 +8,29 @@ let app = new PIXI.Application({
 document.body.appendChild(app.view);
 
 let url;
-let env = 'notlocal';
+let env = 'local';
 
 if (env === 'local') url = "http://localhost:8000";
 else url = "https://raw.githubusercontent.com/ciciZamri/cicizamri.github.io/master/recycle%20game";
 
 let assetList = [
     `${url}/assets/maincharacter.png`,
-    `${url}/assets/homepagebg.png`,
-    `${url}/assets/house.png`,
-    `${url}/assets/riverbtn.png`,
-    `${url}/assets/fieldbtn.png`,
-    `${url}/assets/shopbtn.png`,
-    `${url}/assets/riverbg.png`,
-    `${url}/assets/fieldbg.png`,
-    `${url}/assets/shopbg.png`,
-    `${url}/assets/c1l1.png`,
-    `${url}/assets/c2l1.png`,
-    `${url}/assets/c3l1.png`,
-    `${url}/assets/homebtn.png`,
-    `${url}/assets/bottle.png`,
-    `${url}/assets/can.png`,
-    `${url}/assets/paper.png`,
+    `${url}/asset2/homepagebg.png`,
+    `${url}/asset2/house.png`,
+    `${url}/asset2/riverbtn.png`,
+    //`${url}/asset2/bus.png`,
+    `${url}/asset2/bus.png`,
+    `${url}/asset2/shopbtn.png`,
+    `${url}/asset2/riverbg.jpg`,
+    `${url}/asset2/fieldbg.png`,
+    `${url}/asset2/shopbg.jpg`,
+    `${url}/asset2/c1l1.png`,
+    `${url}/asset2/c2l1.png`,
+    `${url}/asset2/c3l1.png`,
+    `${url}/asset2/homebtn.png`,
+    `${url}/asset2/bottle.png`,
+    `${url}/asset2/can.png`,
+    `${url}/asset2/paper.png`,
     `${url}/assets/inventorycontainer.png`
 ];
 
@@ -42,7 +43,7 @@ let player;
 let homepagebg;
 let house;
 let riverbtn;
-let fieldbtn;
+let bus;
 let shopbtn;
 let riverbg;
 let fieldbg;
@@ -56,21 +57,22 @@ const homepage = new PIXI.Container();
 const river = new PIXI.Container();
 const field = new PIXI.Container();
 const shop = new PIXI.Container();
+let inventoryContainer = new PIXI.Container();
 
 function setup() {
     const playerSprite = new PIXI.Sprite(PIXI.loader.resources[`${url}/assets/maincharacter.png`].texture);
-    homepagebg = new PIXI.Sprite(PIXI.loader.resources[`${url}/assets/homepagebg.png`].texture);
-    house = new PIXI.Sprite(PIXI.loader.resources[`${url}/assets/house.png`].texture);
-    riverbtn = new PIXI.Sprite(PIXI.loader.resources[`${url}/assets/riverbtn.png`].texture);
-    fieldbtn = new PIXI.Sprite(PIXI.loader.resources[`${url}/assets/fieldbtn.png`].texture);
-    shopbtn = new PIXI.Sprite(PIXI.loader.resources[`${url}/assets/shopbtn.png`].texture);
-    riverbg = new PIXI.Sprite(PIXI.loader.resources[`${url}/assets/riverbg.png`].texture);
-    fieldbg = new PIXI.Sprite(PIXI.loader.resources[`${url}/assets/fieldbg.png`].texture);
-    shopbg = new PIXI.Sprite(PIXI.loader.resources[`${url}/assets/shopbg.png`].texture);
-    c1l1 = new PIXI.Sprite(PIXI.loader.resources[`${url}/assets/c1l1.png`].texture);
-    c2l1 = new PIXI.Sprite(PIXI.loader.resources[`${url}/assets/c2l1.png`].texture);
-    c3l1 = new PIXI.Sprite(PIXI.loader.resources[`${url}/assets/c3l1.png`].texture);
-    homebtn = new PIXI.Sprite(PIXI.loader.resources[`${url}/assets/homebtn.png`].texture);
+    homepagebg = new PIXI.Sprite(PIXI.loader.resources[`${url}/asset2/homepagebg.png`].texture);
+    house = new PIXI.Sprite(PIXI.loader.resources[`${url}/asset2/house.png`].texture);
+    riverbtn = new PIXI.Sprite(PIXI.loader.resources[`${url}/asset2/riverbtn.png`].texture);
+    bus = new PIXI.Sprite(PIXI.loader.resources[`${url}/asset2/bus.png`].texture);
+    shopbtn = new PIXI.Sprite(PIXI.loader.resources[`${url}/asset2/shopbtn.png`].texture);
+    riverbg = new PIXI.Sprite(PIXI.loader.resources[`${url}/asset2/riverbg.jpg`].texture);
+    fieldbg = new PIXI.Sprite(PIXI.loader.resources[`${url}/asset2/fieldbg.png`].texture);
+    shopbg = new PIXI.Sprite(PIXI.loader.resources[`${url}/asset2/shopbg.jpg`].texture);
+    c1l1 = new PIXI.Sprite(PIXI.loader.resources[`${url}/asset2/c1l1.png`].texture);
+    c2l1 = new PIXI.Sprite(PIXI.loader.resources[`${url}/asset2/c2l1.png`].texture);
+    c3l1 = new PIXI.Sprite(PIXI.loader.resources[`${url}/asset2/c3l1.png`].texture);
+    homebtn = new PIXI.Sprite(PIXI.loader.resources[`${url}/asset2/homebtn.png`].texture);
     //inventoryContainer = new PIXI.Sprite(PIXI.loader.resources[`${url}/assets/inventorycontainer.png`].texture);
 
     player = new GameObject(playerSprite);
@@ -79,13 +81,13 @@ function setup() {
     homepagebg.scale.set(app.screen.width / homepagebg.width, app.screen.width / homepagebg.width);
     playerSprite.scale.set(0.07, 0.07);
     playerSprite.anchor.set(0.5, 0.5);
-    house.scale.set(0.3, 0.3);
+    house.scale.set(0.5, 0.5);
     house.anchor.set(0.5, 0.5);
-    riverbtn.scale.set(0.3, 0.3);
+    riverbtn.scale.set(0.9, 0.9);
     riverbtn.anchor.set(0.5, 0.5);
-    fieldbtn.scale.set(0.3, 0.3);
-    fieldbtn.anchor.set(0.5, 0.5);
-    shopbtn.scale.set(0.2, 0.2);
+    bus.scale.set(0.3, 0.3);
+    bus.anchor.set(0.5, 0.5);
+    shopbtn.scale.set(0.3, 0.3);
     shopbtn.anchor.set(0.5, 0.5);
     homebtn.scale.set(0.2, 0.2);
     homebtn.anchor.set(0.5, 0.5);
@@ -93,20 +95,20 @@ function setup() {
     riverbg.scale.set(app.screen.width / riverbg.width, app.screen.width / riverbg.width);
     fieldbg.scale.set(app.screen.width / fieldbg.width, app.screen.width / fieldbg.width);
     shopbg.scale.set(app.screen.width / shopbg.width, app.screen.width / shopbg.width);
-    c1l1.scale.set(0.15, 0.15);
+    c1l1.scale.set(0.2, 0.2);
     c1l1.anchor.set(0.5, 0.5);
-    c2l1.scale.set(0.15, 0.15);
+    c2l1.scale.set(0.25, 0.25);
     c2l1.anchor.set(0.5, 0.5);
-    c3l1.scale.set(0.15, 0.15);
+    c3l1.scale.set(0.17, 0.17);
     c3l1.anchor.set(0.5, 0.5);
-    player.gameobj.position.set(app.screen.width / 2, 200);
+    player.gameobj.position.set(400, 350);
     // Player.newposition[0] = player.gameobj.x;
     // Player.newposition[1] = player.gameobj.y;
 
     homepage.addChild(homepagebg);
     homepage.addChild(house);
     homepage.addChild(riverbtn);
-    homepage.addChild(fieldbtn);
+    homepage.addChild(bus);
     homepage.addChild(shopbtn);
     homepage.addChild(c1l1);
     homepage.addChild(c2l1);
@@ -125,10 +127,38 @@ function setup() {
     });
 
     Scene.loadhomepage();
+
+    for(let i = 1 ; i<=3 ; i++){
+        let s;
+        let con = new PIXI.Sprite(PIXI.loader.resources[`${url}/assets/inventorycontainer.png`].texture);
+        con.scale.set(0.35, 0.35);
+        con.anchor.set(0.5, 0.5);
+        con.position.set(1000-i*con.width, 550-50);
+        if(i === 1){
+            bottleCountText.style.left = `${(1000-i*con.width)}px`;
+            s = new PIXI.Sprite(PIXI.loader.resources[`${url}/asset2/bottle.png`].texture);
+            s.scale.set(0.07, 0.07);
+        }
+        else if(i === 2){
+            canCountText.style.left = `${(1000-i*con.width)}px`;
+            s = new PIXI.Sprite(PIXI.loader.resources[`${url}/asset2/can.png`].texture);
+            s.scale.set(0.08, 0.08);
+        }
+        else{
+            paperCountText.style.left = `${(1000-i*con.width)}px`;
+            s = new PIXI.Sprite(PIXI.loader.resources[`${url}/asset2/paper.png`].texture);
+            s.scale.set(0.03, 0.03);
+        }
+        s.position.set(1000-i*con.width-20, 550-75);
+        inventoryContainer.addChild(con);
+        inventoryContainer.addChild(s);
+    }
+    app.stage.addChild(inventoryContainer);
     app.stage.addChild(player.gameobj);
 
     riverbtn.on('pointerdown', Scene.gotoriver);
-    fieldbtn.on('pointerdown', Scene.gotofield);
+    bus.on('pointerdown', Scene.gotofield);
     shopbtn.on('pointerdown', Scene.gotoshop);
+    ItemManager.initialize(30);
     gameloop();
 }
