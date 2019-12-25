e376c23a59f91ba89b1cb821da6312c4675d52e0
cici zamri <hammiziezamri@gmail.com>
2019-12-25 08:35:32 +0800

    update physic.js

--- physicsbody/js/physics.js
+++ physicsbody/js/physics.js
@@ -100,7 +100,7 @@ class ParticlesManager {
     static createParticles(count, view) {
         for (let i = 0; i < count; i++) {
             const particle = view.addObject();
-            ParticlesManager.particles.push(new PhysicsBody(particle, Math.random() * width - width / 2, Math.random() * (height / -2)));
+            ParticlesManager.particles.push(new PhysicsBody(particle, Math.random() * width - width / 2, Math.random() * height - height / 2));
         }
     }
 
