bc78da09b158f632926279a728adce5eeeda758a
cici <hammiziezamri@gmail.com>
2020-01-05 12:28:16 +0800

    start virtual assistant section

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -6,5 +6,29 @@ app = Flask(__name__)
 def home():
     return 'hello'
 
+@app.route('/')
+def add_textcorpus():
+    pass
+
+@app.route('/')
+def get_textcorpus():
+    pass
+
+@app.route('/chitchat/add_data')
+def add_chitchat_dataset():
+    pass
+
+@app.route('/chitchat/get_data')
+def get_chitchat_dataset():
+    pass
+
+@app.route('/speech/add_data')
+def add_speech_dataset():
+    pass
+
+@app.route('/speech/get_data')
+def get_speech_dataset():
+    pass
+
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ virtual_assistant/js/language_model_dataset.js
@@ -0,0 +1,7 @@
+const dataForm = document.querySelector("#data-form");
+const addBtn = document.querySelector("add-data-btn");
+const saveBtn = document.querySelector("save-data-btn");
+
+addBtn.addEventListener('click', (e)=>{
+    dataForm.getElementsByClassName.display = "block";
+});
