dd75d3c33284af156fb7e33e23e3088721ae3616
cici <hammiziezamri@gmail.com>
2020-04-30 15:42:50 +0800

    start differential equations note

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ document.querySelector(".nav-bar-menu-btn").addEventListener('click', (e)=>{
 
 document.querySelector(".nav-bar-close-btn").addEventListener('click', (e)=>{
     console.log("close");
-    document.querySelector(".nav-bar-actions").style.transform = "translateX(150px)";
+    document.querySelector(".nav-bar-actions").style.transform = "translateX(200px)";
 });
 
 //menu button
