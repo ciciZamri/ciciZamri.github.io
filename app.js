58e81b857127592cd5c0bc96125b6a122b4c8de0
cici <hammiziezamri@gmail.com>
2020-05-01 16:38:51 +0800

    add random fun page

--- app.js
+++ app.js
@@ -7,4 +7,13 @@ document.querySelector(".nav-bar-close-btn").addEventListener('click', (e)=>{
     document.querySelector(".nav-bar-actions").style.transform = "translateX(200px)";
 });
 
-//menu button
+let lastY = window.scrollY;
+window.addEventListener('scroll', (e)=>{
+    if(window.scrollY > lastY){
+        document.querySelector(".nav-bar").style.transform = "translateY(-70px)";
+    }
+    else{
+        document.querySelector(".nav-bar").style.transform = "translateY(0px)";
+    }
+    lastY = window.scrollY;
+});
