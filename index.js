b627b6a1f27906fe4cd38387c7baa9dd983aae64
cicizamri <hammiziezamri@gmail.com>
2021-10-15 22:49:04 +0800

    rewrite

---
+++ index.js
@@ -0,0 +1,11 @@
+// import AOS from 'aos';
+
+AOS.init({
+    duration: 1000
+});
+
+const actionButton = document.getElementById("action-btn");
+
+actionButton.addEventListener('click', (e)=>{
+    scrollTo(0, window.innerHeight);
+});
